a3b20509cd0284d2f922099b28191eddbdba2597
Benson Zheng <bensonzheng2003@gmail.com>
2024-12-23 19:34:43 -0500

    comment out logging user

--- app/(Home)/home.tsx
+++ app/(Home)/home.tsx
@@ -5,25 +5,25 @@ import SignOutButton from '../../components/sign-out';
 import { Redirect } from 'expo-router'; 
 
 export default function HomePage() {
-  const { user, isLoaded } = useUser(); // Check if the user is signed in
-    console.log(user);
-  useEffect(() => {
-    if (!isLoaded) {
-      // You can handle the loading state here if necessary, e.g., log or show loading spinner
-      console.log('User data is still loading...');
+    const { user, isLoaded } = useUser(); // Check if the user is signed in
+    //console.log(user);
+    useEffect(() => {
+        if (!isLoaded) {
+        // You can handle the loading state here if necessary, e.g., log or show loading spinner
+        console.log('User data is still loading...');
+        }
+    }, [isLoaded]);
+
+    if (isLoaded && !user) {
+        return <Redirect href="/sign-in" />;
     }
-  }, [isLoaded]);
 
-  if (isLoaded && !user) {
-    return <Redirect href="/sign-in" />;
-  }
-
-  return (
-    <View className="flex-1 justify-center items-center">
-        <Text>
-            Hello {user?.emailAddresses[0].emailAddress} 
-        </Text>
-        <SignOutButton />
-    </View>
-  );
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text>
+                Hello {user?.emailAddresses[0].emailAddress} 
+            </Text>
+            <SignOutButton />
+        </View>
+    );
 }
